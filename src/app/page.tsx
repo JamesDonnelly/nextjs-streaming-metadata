2078db6f42c6a97d18a399981193db788ff2f507
James Donnelly <2203377+JamesDonnelly@users.noreply.github.com>
2025-01-22 22:08:55 +0000

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import type { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Example title',
+  description: 'Example description',
+}
 
 export default function Home() {
   return (
