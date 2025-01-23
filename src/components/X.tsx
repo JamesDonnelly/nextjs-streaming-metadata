30587927b5be11155e4ef509001c6ce1882a562f
James Donnelly <2203377+JamesDonnelly@users.noreply.github.com>
2025-01-23 12:00:44 +0000

    try removing cookie setter

--- src/components/X.tsx
+++ src/components/X.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import Cookies from 'js-cookie';
 
 const X: React.FC<React.PropsWithChildren> = ({ children }) => {
-  Cookies.set('foobar', 'barfoo');
   Cookies.get('foobar');
   return <div>{children}</div>;
 }
